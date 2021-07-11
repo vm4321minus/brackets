cc27abe54f8df2721baeb68867bf365765497424
vm4321minus <>
2021-07-11 03:16:42 +0300

    feat: breackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 > 0) return false;
+
+  let strone = str.length;
+  let strtwo = bracketsConfig.length;
+
+  for (let j = 0; j < strone / 2; j++) {
+    for (let i = 0; i < strtwo; i++) {
+      str = str.replace(bracketsConfig[i].join(''), '');
+    }
+  }
+  return str.length > 0 ? false : true;
 }
